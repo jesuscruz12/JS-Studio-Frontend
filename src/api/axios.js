b75f414ec89a80087c4f41a792801159caed170b
jesuscruz12 <20221059@uthh.edu.mx>
2026-01-23 15:58:47 -0600

    actualizacion de ruta

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://sj-studio-backend.onrender.com/api",
 });
 
 // Interceptor para enviar token automáticamente
